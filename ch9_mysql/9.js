0345ed5380b40c6d5f3ecda8d97551bfd35c268b
lawiparadise <lawiparadise@gmail.com>
2017-01-01 02:24:19 +0900

    to the 9 end

--- ch9_mysql/9.js
+++ ch9_mysql/9.js
@@ -1,1 +1,16 @@
-sss
+var mysql = require('mysql');
+
+var client = mysql.createConnection({
+    user: 'root',
+    password: '1234',
+    database: 'Company'
+});
+
+// client.query('USE Company');
+client.query('SELECT * FROM products', function (error, result, fields) {
+    if(error){
+        console.log('에러발생');
+    } else{
+        console.log(result);
+    }
+});
---
+++ ch9_mysql/app.js
@@ -0,0 +1,70 @@
+var fs = require('fs');
+var ejs = require('ejs');
+var http = require('http');
+var mysql = require('mysql');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var router = express.Router();
+
+var client = mysql.createConnection({
+    user : 'root',
+    password : '1234',
+    database : 'Company'
+});
+
+app.use(router);
+app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+http.createServer(app).listen(52273, function () {
+    console.log('server running at 52273');
+});
+
+app.get('/', function (req, res) {
+    fs.readFile('list.html', 'utf-8', function (error, data) {
+        client.query('SELECT * FROM products', function (error, results) {
+            res.send(ejs.render(data, {
+                data: results
+            }));
+        });
+    });
+});
+
+app.get('/delete/:id', function (req, res) {
+    client.query('DELETE FROM products WHERE id=?', [req.params.id], function () {
+        res.redirect('/');
+    })
+});
+
+app.get('/insert', function (req, res) {
+    fs.readFile('insert.html', 'utf-8', function (error, data) {
+        res.send(data);
+    });
+});
+
+app.post('/insert', function (req, res) {
+    var body = req.body;
+
+    client.query('INSERT INTO products (name, modelnumber, series) VALUES (?, ?, ?)', [body.name, body.modelnumber, body.series], function () {
+        res.redirect('/');
+    });
+});
+
+app.get('/edit/:id', function (req, res) {
+    fs.readFile('edit.html', 'utf-8', function (error, data) {
+        client.query('SELECT * FROM products WHERE id= ?', [req.params.id], function (error, result) {
+            res.send(ejs.render(data, {data:result[0]}));
+        });
+    });
+});
+
+app.post('/edit/:id', function (req, res) {
+    var body = req.body;
+
+    client.query('UPDATE products SET name=?, modelnumber=?, series=? WHERE id=?', [body.name, body.modelnumber, body.series, req.params.id], function () {
+        res.redirect('/');
+    });
+});
